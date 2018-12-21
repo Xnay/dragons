0ec197288bf79d9e9898b6032be066bee0997148
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-12-21 10:36:49 -0500

    Beginning of pathfinding

--- path-finding.js
+++ path-finding.js
@@ -1,4 +1,5 @@
 const directions = require("./directions/directions");
+const directionsEnum = require("./directions/directions-enum");
 
 /**
  * Both from and to are Points.
@@ -13,9 +14,19 @@ function pathFinding(map, from, to) {
 
     if (Math.abs(distX) > Math.abs(distY)) {
         if (distX > 0) {
-            //move =
+            move = directionsEnum.EAST;
+        } else {
+            move = directionsEnum.WEST;
+        }
+    } else {
+        if (distY > 0) {
+            move = directionsEnum.NORTH;
+        } else {
+            move = directionsEnum.SOUTH;
         }
     }
+
+    return move;
 }
 
 module.exports = pathFinding;
